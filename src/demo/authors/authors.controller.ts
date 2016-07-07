8c67c45d8a005195dbcfad5599f825bf8deefeaa
Pablo Reyes <reyesoft@gmail.com>
2016-07-06 21:17:31 -0300

    better console.log on demo

--- src/demo/authors/authors.controller.ts
+++ src/demo/authors/authors.controller.ts
@@ -11,7 +11,7 @@ module demoApp {
             this.authors = AuthorsService.all(
                 // { include: ['books', 'photos'] },
                 success => {
-                    console.log('success authors controll', success);
+                    console.log('success authors controll', this.authors);
                 },
                 error => {
                     console.log('error authors controll', error);
